f2b5c5e56d130e589b17c9bba0b5f2824bdcdc4c
snutan11 <nutan>
2025-03-28 10:31:35 +0530

    adding folder structure

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,15 @@
 const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv");
+// const cors = require("cors");
+// const dotenv = require("dotenv");
 
-dotenv.config(); // Load environment variables from .env file
+// dotenv.config(); // Load environment variables from .env file
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
 // Test Route
 app.get("/", (req, res) => {
